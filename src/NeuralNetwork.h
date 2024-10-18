9c6e687a8559f197d9f3141c24583a12f8bca170
‮ <gxousos@gmail.com>
2024-10-18 13:38:03 +0300

    v2.4.4-beta

--- src/NeuralNetwork.h
+++ src/NeuralNetwork.h
@@ -58,7 +58,7 @@
 #ifndef NeuralNetwork_h
 #define NeuralNetwork_h
 
-#define __NN_VERSION__ "VERSION: 2.4.1"\n
+#define __NN_VERSION__ "VERSION: 2.4.4"\n
 
 // - That gives you access to the standard types and constants of the Arduino language.
 #include "Arduino.h"
